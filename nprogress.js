4f4215dba688dab14ea001724b4f5d4e63f5b6ae
ramy rais <ramy.rais@gmail.com>
2018-09-23 20:44:35 +0200

    add id nprogress for compatibility with older version

--- nprogress.js
+++ nprogress.js
@@ -236,6 +236,7 @@
     addClass(document.documentElement, 'nprogress-busy');
 
     var progress = document.createElement('div');
+    progress.id = '#nprogress';
     progress.className = 'nprogress';
     progress.innerHTML = Settings.template;
 
